ef87127f9bb03740c9c66e42d3b4f9e4fd220963
noceive <nocivescience@gmail.com>
2023-03-05 01:44:04 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.clear();
+const ringCount = 42;
+const ringPointCount=80
+const lineSpacing = 4;
+const ringSpacing = 2;
+const lineColor = "#fa3";
+const ringColor = "#3af";
+const modeTransitionDuration = 3000;
+const stage = new Stage({
+    container: document.body
+});
